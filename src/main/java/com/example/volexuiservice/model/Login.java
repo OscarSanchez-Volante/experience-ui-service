8e600e0b21c5ec5fe5f77f035ccdb67a4902c888
JLongines <juan.longines@volantetech.com>
2022-11-04 17:11:24 -0600

    Login mock service

---
+++ src/main/java/com/example/volexuiservice/model/Login.java
@@ -0,0 +1,13 @@
+package com.example.volexuiservice.model;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+public class Login {
+    @NotNull(message="The Email cannot be null")
+    private String email;
+    @NotNull(message="The Password cannot be null")
+    private String password;
+}
